9437f3fb4121ff2a3906cbb8f7151252560b24ad
Daniel Kulla <dkulla129@gmail.com>
2023-05-17 10:18:55 -0400

    install iptables-persistent
    iptables-persistent creates the configuration files for iptables when
    you install it. I don't think I want that, because the install process
    becomes interactive, which makes it possible for the user to
    misconfigure something.

--- bin/bridge_wlan0_to_eth0.sh
+++ bin/bridge_wlan0_to_eth0.sh
@@ -7,13 +7,12 @@ source "$DIR/echoerr.sh"
 # shellcheck source=./install_package_if_absent.sh
 source "$DIR/install_package_if_absent.sh"
 
-echoerr 'making sure dnsmasq and iptables-persistent are installed'
+echoerr 'making sure dnsmasq and hostapd are installed'
 install_package_if_absent 'dnsmasq'
-install_package_if_absent 'iptables-persistent'
+install_package_if_absent 'hostapd'
 
-
-printf 'interface eth0\
-static ip_address=10.0.0.1/24\
+printf 'interface eth0
+static ip_address=10.0.0.1/24
 static domain_name_servers=1.1.1.1,8.8.8.8' | sudo tee -a /etc/dhcpcd.conf > /dev/null
 
 
@@ -21,7 +20,17 @@ echoerr 'enabling ipv4 forwarding in /etc/sysctl.conf'
 sudo sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
 
 echoerr 'configuring dnsmasq'
+echoerr 'making sure dnsmasq waits for dhcpcd before starting up'
+sudo sed -i '/### BEGIN INIT INFO/i\
+# Hack to wait until dhcpcd is ready\
+sleep 10\
+' /etc/init.d/dnsmasq
+
+sudo sed -i 's/# Required-Start: $network $remote_fs $syslog/# Required-Start: $network $remote_fs $syslog $dhcpcd/g' /etc/init.d/dnsmasq
+
 echoerr 'moving aside the old dnsmasq.conf'
 sudo mv /etc/dnsmasq.conf /etc/dnsmasq.conf.backup
 
 
+
+install_package_if_absent 'iptables-persistent'
