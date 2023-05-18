ca83e98198ed833e58d59d56dbbb291e1f46c1e9
Daniel Kulla <dkulla129@gmail.com>
2023-05-18 18:10:38 -0400

    extract the cert creation logic into a function
    idk why I'm still using bash for this. oh well

--- bin/generate_certs.sh
+++ bin/generate_certs.sh
@@ -9,103 +9,92 @@ if ! command -v openssl &> /dev/null; then
   exit 1
 fi
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-PARENT_DIR=$(dirname "$SCRIPT_DIR")
-CERTS_DIR_SUFFIX="etc-nginx-proxy/ssl/certs"
-CA_CREATION_DIR="${PARENT_DIR}/ssl/ca"
-CERT_CREATION_DIR="${PARENT_DIR}/ssl/certs"
+script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+parent_dir=$(dirname "$script_dir")
+ca_creation_dir="${parent_dir}/ssl/ca"
+cert_creation_dir="${parent_dir}/ssl/certs"
+pihome_ca_key="${ca_creation_dir}/pihome-ca.key"
+pihome_ca_pemfile="${ca_creation_dir}/pihome-ca.pem"
 
-if [ -d "$CA_CREATION_DIR"  ] && [ -f "$CA_CREATION_DIR/pihome-ca.pem" ]; then
-  echoerr "it looks like we've already created the root certificate/ \`$CA_CREATION_DIR\` directory. If you need a new \
+
+if [ -d "$ca_creation_dir"  ] && [ -f "$ca_creation_dir/pihome-ca.pem" ]; then
+  echoerr "it looks like we've already created the root certificate/ \`$ca_creation_dir\` directory. If you need a new \
   root certificate, remove this directory."
 else
-  echoerr "creating $CA_CREATION_DIR"
-  mkdir -p "$CA_CREATION_DIR"
+  echoerr "creating $ca_creation_dir"
+  mkdir -p "$ca_creation_dir"
 
   echoerr "creating a local root CA private key for LAN-hosted pages"
-  openssl genrsa -des3 -out "${CA_CREATION_DIR}/pihome-ca.key" 4096
+  openssl genrsa -des3 -out "${pihome_ca_key}" 4096
 
   echoerr "creating a local certificate with that root CA private key"
-  openssl req -x509 -new -nodes -key "${CA_CREATION_DIR}/pihome-ca.key" -sha256 -days 3650 -out "${CA_CREATION_DIR}/pihome-ca.pem" -subj "/CN=pihome-ca.run"
+  openssl req -x509 -new -nodes -key "${pihome_ca_key}" -sha256 -days 3650 -out "${pihome_ca_pemfile}" -subj "/CN=pihome-ca.run"
 
   echoerr "copying the pihome-ca cert to the ca-certificates dir"
-  sudo cp "${CA_CREATION_DIR}/pihome-ca.pem" /usr/local/share/ca-certificates/pihome-ca.crt
+  sudo cp "${pihome_ca_pemfile}" /usr/local/share/ca-certificates/pihome-ca.crt
 
   echoerr "updating the certificates store"
   sudo update-ca-certificates
 fi
 
-NGINX_CERT_CREATION_DIR="${CERT_CREATION_DIR}/nginx"
+function build_certs() {
+  local certs_dirname=$1
+  local cert_prefix=$2
+  local delimited_sans=$3
+  local ca_pemfile=$4
+  local ca_key=$5
 
-if [ -d "$NGINX_CERT_CREATION_DIR" ]; then
-  echoerr "it looks like an nginx certificates directory already exists: \`$NGINX_CERT_CREATION_DIR\`. If you need new certificates \
-  remove this directory."
-else
-  echoerr "creating an nginx certificate from our ca root certificate"
-  mkdir -p "$NGINX_CERT_CREATION_DIR"
+  if [ -d "$certs_dirname" ]; then
+    echoerr "it looks like a \`$cert_prefix\` certificates directory already exists: \`$certs_dirname\`. If you need new certificates \
+    remove this directory."
+  else
+    echoerr "creating a \`$cert_prefix\` certificate from our ca root certificate (\`$ca_pemfile\`)"
+    mkdir -p "$certs_dirname"
 
-  echoerr "creating self signed x509 cert for pihome nginx-proxy to use"
+    echoerr "creating a private key for \`$cert_prefix\`."
 
-  openssl genrsa -out "${NGINX_CERT_CREATION_DIR}/pihome.run.key" 4096
-  openssl req -new -key "${NGINX_CERT_CREATION_DIR}/pihome.run.key" \
-    -subj "/C=US/ST=MA/CN=pihome.run" \
-    -out "${NGINX_CERT_CREATION_DIR}/pihome.run.csr"
+    openssl genrsa -out "${certs_dirname}/${cert_prefix}.key" 4096
+    openssl req -new -key "${certs_dirname}/${cert_prefix}" \
+      -subj "/C=US/ST=MA/CN=pihome.run" \
+      -out "${certs_dirname}/${cert_prefix}.csr"
 
-  echoerr "creating the x509 cert extension config file to attach the SANs"
-  printf '%s\n' \
-    "authorityKeyIdentifier=keyid,issuer" \
-    "basicConstraints=CA:FALSE" \
-    "keyUsage = digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment" \
-    "subjectAltName = @alt_names" \
-    "" \
-    "[alt_names]" \
-    "DNS.1 = pihome.run" \
-    "DNS.2 = pihole.pihome.run" \
-    "DNS.3 = homebridge.pihome.run" \
-    "DNS.4 = zigbee2mqtt.pihome.run" \
-  > "${NGINX_CERT_CREATION_DIR}/pihome.run.ext"
+    echoerr "creating the x509 cert extension config file to attach the SANs"
+    local extfile_name="${cert_prefix}.ext"
 
-  openssl x509 -trustout -req -in "${NGINX_CERT_CREATION_DIR}/pihome.run.csr" -CA "${CA_CREATION_DIR}/pihome-ca.pem" -CAkey "${CA_CREATION_DIR}/pihome-ca.key" \
-  -CAcreateserial -out "${NGINX_CERT_CREATION_DIR}/pihome.run.crt" -days 3650 -sha256 -extfile "${NGINX_CERT_CREATION_DIR}/pihome.run.ext"
+    printf '%s\n' \
+      "authorityKeyIdentifier=keyid,issuer" \
+      "basicConstraints=CA:FALSE" \
+      "keyUsage = digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment" \
+      "subjectAltName = @alt_names" \
+      "" \
+      "[alt_names]" \
+      "${delimited_sans}" \
+  > "${certs_dirname}/${extfile_name}"
 
-  echoerr "done creating the self-signed x509 cert. make sure this cert is in the appropriate \
-  place for nginx-proxy to find (probably $CERTS_DIR_SUFFIX within whatever dir you run \
-  \`docker compose\` from). Also add the pihome-ca.pem to whichever computers you plan to access the \
-  pihome UIs from."
+    openssl x509 -trustout -req -in "${certs_dirname}/server.csr" -CA "${ca_pemfile}" -CAkey "${ca_key}" \
+    -CAcreateserial -out "${certs_dirname}/${cert_prefix}.crt" -days 3650 -sha256 -extfile "${certs_dirname}/${extfile_name}"
+  fi
+}
 
-  echoerr 'Copy the root certificate to your machine. From your machine, run:'
-  echoerr "        scp $(whoami)@$(hostname).local:${CA_CREATION_DIR}/pihome-ca.pem ~/Downloads"
-  echoerr 'then tell your OS to recognize this root certificate and trust it (e.g. with Keychain Access on MacOS).'
-fi
+function build_dns_sans_block() {
+    local cert_sans=("$@")    
+    local dns_sans=()
 
-MOSQUITTO_CERT_CREATION_DIR="${CERT_CREATION_DIR}/mosquitto"
+    for san_index in "${!cert_sans[@]}"; do
+      dns_sans+=("DNS.$((san_index + 1)) = ${cert_sans[$san_index]}")
+    done
 
-if [ -d "$MOSQUITTO_CERT_CREATION_DIR" ]; then
-  echoerr "it looks like a mosquitto certificates directory already exists: \`$MOSQUITTO_CERT_CREATION_DIR\`. If you need new certificates \
-  remove this directory."
-else
-  echoerr "creating a mosquitto broker certificate from our ca root certificate"
-  mkdir -p "$MOSQUITTO_CERT_CREATION_DIR"
+    printf '%s\n' "${dns_sans[@]}" 
+}
 
-  echoerr "creating self signed x509 cert for mosquitto to use"
 
-  openssl genrsa -out "${MOSQUITTO_CERT_CREATION_DIR}/server.key" 4096
-  openssl req -new -key "${MOSQUITTO_CERT_CREATION_DIR}/server.key" \
-    -subj "/C=US/ST=MA/CN=pihome.run" \
-    -out "${MOSQUITTO_CERT_CREATION_DIR}/server.csr"
+nginx_cert_creation_dir="${cert_creation_dir}/nginx"
+nginx_cert_sans="$(build_dns_sans_block 'pihome.run' 'pihole.pihome.run' 'homebridge.pihome.run' 'zigbee2mqtt.pihome.run')"
+build_certs "$nginx_cert_creation_dir" 'pihome.run' "$nginx_cert_sans" "${pihome_ca_pemfile}" "${pihome_ca_key}"
 
-  echoerr "creating the x509 cert extension config file to attach the SANs"
-  printf '%s\n' \
-    "authorityKeyIdentifier=keyid,issuer" \
-    "basicConstraints=CA:FALSE" \
-    "keyUsage = digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment" \
-    "subjectAltName = @alt_names" \
-    "" \
-    "[alt_names]" \
-    "DNS.1 = pihome.run" \
-> "${MOSQUITTO_CERT_CREATION_DIR}/mosquitto.run.ext"
+mosquitto_cert_sans="$(build_dns_sans_block 'pihome.run')"
+mosquitto_server_cert_creation_dir="${cert_creation_dir}/mosquitto-server"
+build_certs "$mosquitto_server_cert_creation_dir" 'server' "$mosquitto_cert_sans" "${pihome_ca_pemfile}" "${pihome_ca_key}"
 
-  openssl x509 -trustout -req -in "${MOSQUITTO_CERT_CREATION_DIR}/server.csr" -CA "${CA_CREATION_DIR}/pihome-ca.pem" -CAkey "${CA_CREATION_DIR}/pihome-ca.key" \
-  -CAcreateserial -out "${MOSQUITTO_CERT_CREATION_DIR}/server.crt" -days 3650 -sha256 -extfile "${MOSQUITTO_CERT_CREATION_DIR}/mosquitto.run.ext"
-
-fi
+mosquitto_client_cert_creation_dir="${cert_creation_dir}/mosquitto-client"
+build_certs "$mosquitto_client_cert_creation_dir" 'client' "$mosquitto_cert_sans" "${pihome_ca_pemfile}" "${pihome_ca_key}"
