a64d8f09dad898bfd4ac2e333d68534bfec4e336
Daniel Kulla <dkulla129@gmail.com>
2023-06-25 15:21:06 -0400

    copy the ca cert too

--- docker/zigbee2mqtt/bin/copy_certs.sh
+++ docker/zigbee2mqtt/bin/copy_certs.sh
@@ -10,6 +10,19 @@ main_scripts_dir="$project_root_dir/bin"
 # shellcheck source=../../../bin/echoerr.sh
 source "$main_scripts_dir/echoerr.sh"
 
+
+function ensure_file_exists() {
+  local file_path=$1
+  if [ ! -f "$file_path" ]; then
+    echoerr "unable to find required file ${file_path}. Ensure this file exists before proceeding"
+    exit 1
+  fi
+}
+
+#ensure that the ca cert has been created
+pihome_ca_cert=${project_root_dir}/ssl/ca/pihome-ca.pem
+ensure_file_exists "$pihome_ca_cert"
+
 # ensure that all of the certs have been created
 certs_root_dir="${project_root_dir}/ssl/certs"
 mosquitto_server_certs_dir="$certs_root_dir/mosquitto-server"
@@ -20,14 +33,6 @@ mosquitto_client_certs_dir="$certs_root_dir/mosquitto-client"
 mosquitto_client_key="$mosquitto_client_certs_dir/client.key"
 mosquitto_client_cert="$mosquitto_client_certs_dir/client.crt"
 
-function ensure_file_exists() {
-  local file_path=$1
-  if [ ! -f "$file_path" ]; then
-    echoerr "unable to find required file ${file_path}. Ensure this file exists before proceeding"
-    exit 1
-  fi
-}
-
 ensure_file_exists "$mosquitto_server_key"
 ensure_file_exists "$mosquitto_server_cert"
 ensure_file_exists "$mosquitto_client_key"
@@ -37,10 +42,12 @@ mosquitto_server_cert_destination_dir="$zigbee2mqtt_root_dir/mosquitto-ssl"
 mkdir -p "$mosquitto_server_cert_destination_dir"
 cp "$mosquitto_server_key" "$mosquitto_server_cert_destination_dir"
 cp "$mosquitto_server_cert" "$mosquitto_server_cert_destination_dir"
+cp "$pihome_ca_cert" "$mosquitto_server_cert_destination_dir"
 
-mosquitto_client_certs_destination_dir="$zigbee2mqtt_root_dir/zigbee2mqtt-ssl"
-mkdir -p "$mosquitto_client_certs_destination_dir"
-cp "$mosquitto_client_key" "$mosquitto_client_certs_destination_dir"
-cp "$mosquitto_client_cert" "$mosquitto_client_certs_destination_dir"
+mosquitto_client_cert_destination_dir="$zigbee2mqtt_root_dir/zigbee2mqtt-ssl"
+mkdir -p "$mosquitto_client_cert_destination_dir"
+cp "$mosquitto_client_key" "$mosquitto_client_cert_destination_dir"
+cp "$mosquitto_client_cert" "$mosquitto_client_cert_destination_dir"
+cp "$pihome_ca_cert" "$mosquitto_client_cert_destination_dir"
 
 echoerr "done copying mosquitto server and client certificates"
