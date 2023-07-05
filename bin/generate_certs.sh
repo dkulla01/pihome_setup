cdca9692dbab5cdd26ead6b059785916a347f07b
Daniel Kulla <dkulla129@gmail.com>
2023-07-05 18:15:34 -0400

    generate all of the certs
    and don't prompt for passwords every single time

--- bin/generate_certs.sh
+++ bin/generate_certs.sh
@@ -21,6 +21,13 @@ pihome_ca_pemfile="${ca_creation_dir}/pihome-ca.pem"
 pihome_sans_domains_file="${script_dir}/pihome_domains.json"
 mosquitto_sans_domains_file="${script_dir}/mosquitto_domains.json"
 
+read -s -p "enter your desired ca private key password: " ca_key_password
+read -s -p "confirm your ca private key password: " confirm_password
+
+if [[ "$ca_key_password" != "$confirm_password" ]]; then
+  echoerr "passwords do not match. exiting."
+  exit 1
+fi
 
 if [ -d "$ca_creation_dir"  ] && [ -f "$ca_creation_dir/pihome-ca.pem" ]; then
   echoerr "it looks like we've already created the root certificate: \`$ca_creation_dir\` directory." \
@@ -30,13 +37,13 @@ else
   mkdir -p "$ca_creation_dir"
 
   echoerr "creating a local root CA private key for LAN-hosted pages"
-  openssl genrsa -des3 -out "${pihome_ca_key}" 4096
+  openssl genrsa -des3 -out "$pihome_ca_key" -passout "pass:${ca_key_password}" 4096
 
   echoerr "creating a local certificate with that root CA private key"
-  openssl req -x509 -new -nodes -key "${pihome_ca_key}" -sha256 -days 3650 -out "${pihome_ca_pemfile}" -subj "/CN=pihome-ca.run"
+  openssl req -x509 -new -nodes -key "$pihome_ca_key" -passin "pass:${ca_key_password}" -sha256 -days 3650 -out "$pihome_ca_pemfile" -subj "/CN=pihome-ca.run"
 
   echoerr "copying the pihome-ca cert to the ca-certificates dir"
-  sudo cp "${pihome_ca_pemfile}" /usr/local/share/ca-certificates/pihome-ca.crt
+  sudo cp "$pihome_ca_pemfile" /usr/local/share/ca-certificates/pihome-ca.crt
 
   echoerr "updating the certificates store"
   sudo update-ca-certificates
@@ -49,6 +56,7 @@ function build_certs() {
   local domains_json_file=$4
   local ca_pemfile=$5
   local ca_key=$6
+  local ca_key_password=$7
 
   if [ -d "$certs_dirname" ]; then
     echoerr "it looks like a \`$cert_prefix\` certificates directory already exists: \`$certs_dirname\`."\
@@ -84,6 +92,7 @@ function build_certs() {
     cert_file="${certs_dirname}/${cert_prefix}.crt"
     echoerr "creating a certificate: ${cert_file}"
     openssl x509 -req -in "$csr_file" -CA "${ca_pemfile}" -CAkey "${ca_key}" \
+    -passin "pass:${ca_key_password}" \
     -CAcreateserial -out "$cert_file" -days 3650 -sha256 -extfile "${certs_dirname}/${extfile_name}"
   fi
 }
@@ -94,10 +103,12 @@ function build_dns_sans_block() {
 }
 
 traefik_cert_creation_dir="${cert_creation_dir}/traefik"
-build_certs "$traefik_cert_creation_dir" 'pihome.run' 'pihome.run' "$pihome_sans_domains_file" "${pihome_ca_pemfile}" "${pihome_ca_key}"
+build_certs "$traefik_cert_creation_dir" 'pihome.run' 'pihome.run' "$pihome_sans_domains_file" "$pihome_ca_pemfile" "$pihome_ca_key" "$ca_key_password"
 
 mosquitto_server_cert_creation_dir="${cert_creation_dir}/mosquitto-server"
-build_certs "$mosquitto_server_cert_creation_dir" 'server' 'pihome-mqtt-server.run' "$mosquitto_sans_domains_file" "${pihome_ca_pemfile}" "${pihome_ca_key}"
+build_certs "$mosquitto_server_cert_creation_dir" 'server' 'pihome-mqtt-server.run' "$mosquitto_sans_domains_file" "$pihome_ca_pemfile" "$pihome_ca_key" "$ca_key_password"
 
 mosquitto_client_cert_creation_dir="${cert_creation_dir}/mosquitto-client"
-build_certs "$mosquitto_client_cert_creation_dir" 'client' 'pihome-mqtt-client.run' "$mosquitto_sans_domains_file" "${pihome_ca_pemfile}" "${pihome_ca_key}"
+build_certs "$mosquitto_client_cert_creation_dir" 'mqtt-explorer-client' 'pihome-mqtt-client.run' "$mosquitto_sans_domains_file" "$pihome_ca_pemfile" "$pihome_ca_key" "$ca_key_password"
+build_certs "$mosquitto_client_cert_creation_dir" 'homeassistant-client' 'pihome-mqtt-client.run' "$mosquitto_sans_domains_file" "$pihome_ca_pemfile" "$pihome_ca_key" "$ca_key_password"
+build_certs "$mosquitto_client_cert_creation_dir" 'client' 'pihome-mqtt-client.run' "$mosquitto_sans_domains_file" "$pihome_ca_pemfile" "$pihome_ca_key" "$ca_key_password"
