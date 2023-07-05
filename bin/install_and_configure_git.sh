59a2cceb09d6b2f931a34ad2e48e211ca606c9df
dkulla01 <dkulla129@gmail.com>
2023-07-05 13:09:54 -0400

    Apt get instead of apt (#9)
    * switch to apt-get and --assume-yes
    
    the git installer script was choking on expecting user input. this
    should fix that issue
    
    * fix sh condition
    
    plain old vanilla sh does not have the double bracket extended test
    syntax that bash has

--- bin/install_and_configure_git.sh
+++ bin/install_and_configure_git.sh
@@ -9,30 +9,30 @@ echoerr() {
 
 # a copy of install_package_if_absent so we can curl/download the script from raw.githubusercontent.com/...
 install_package_if_absent() {
-  DEP_NAME=$1
+  local dep_name=$1
 
-  if dpkg-query -W -f='${Status}\n' "$DEP_NAME" 2> /dev/null | grep -q 'ok installed'; then
-    echoerr "$DEP_NAME is already installed. nothing to do here."
+  if dpkg-query -W -f='${Status}\n' "$dep_name" 2> /dev/null | grep -q 'ok installed'; then
+    echoerr "$dep_name is already installed. nothing to do here."
   else
-    echoerr "$DEP_NAME is not yet installed. Installing it now."
-    sudo apt-get install --assume-yes "$DEP_NAME"
+    echoerr "$dep_name is not yet installed. Installing it now."
+    sudo apt-get install --assume-yes "$dep_name"
   fi
 }
 
-echoerr "updating and upgrading apt..."
+echoerr "updating and upgrading apt-get..."
 
 
-if ! sudo apt update; then
+if ! sudo apt-get update --assume-yes; then
   echoerr "something went wrong updating apt"
   exit 1
 fi
 
-if ! sudo apt upgrade; then
+if ! sudo apt-get upgrade --assume-yes; then
   echoerr "something went wrong upgrading packages"
   exit 1
 fi
 
-echoerr "apt is up to date. moving on"
+echoerr "apt-get is up to date. moving on"
 
 echoerr "checking git..."
 install_package_if_absent 'git'
@@ -49,7 +49,7 @@ fi
 
 # ssh -T returns 1 on success, other nonzero code on failure
 ssh -o StrictHostKeyChecking=no -T git@github.com 1>/dev/null 2>&1 || EXIT_CODE=$?
-if [[ ${EXIT_CODE} != 1 ]]; then
+if [ "$EXIT_CODE" -ne 1 ]; then
   echoerr "failed to authenticate with github. you need to add your new ssh key to your github account"
   echoerr "> https://docs.github.com/en/repositories/creating-and-managing-repositories/troubleshooting-cloning-errors#check-your-ssh-access"
   exit 1
