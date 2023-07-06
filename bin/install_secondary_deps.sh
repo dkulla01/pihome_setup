dd091d465b9b42767dd19a4b9674279e03998b39
Daniel Kulla <dkulla129@gmail.com>
2023-07-06 14:51:40 -0400

    template out mqtt password/user stuff
    this way, generating the mqtt creds also creates the password file that
    zigbee2mqtt expects. it also gitignores it so I don't accidentally check
    in the credentials.
    
    it also installs yq and direnv to let us template the z2m zigbee-serial
    adapter

--- bin/install_secondary_deps.sh
+++ bin/install_secondary_deps.sh
@@ -27,6 +27,11 @@ install_package_if_absent 'libsqlite3-dev'
 install_package_if_absent 'liblzma-dev'
 install_package_if_absent 'apache2-utils' # needed for htpasswd
 install_package_if_absent 'jq'
+if ! command -v yq ; then
+  echoerr 'installing yq'
+  wget https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 -O /usr/bin/yq &&\
+    chmod +x /usr/bin/yq
+fi
 
 echoerr 'done installing packages required for pyenv to build pythons.'
 
@@ -34,25 +39,45 @@ echoerr 'installing pyenv'
 curl https://pyenv.run | bash
 echoerr "done installing pyenv"
 
-echoerr 'adding pyenv environment variables to .bashrc'
 # we want to put these literal strings (with their variables) into .bashrc
 # without interpolating/evaluating the variables
 # shellcheck disable=SC2016
-{
-  echo 'export PYENV_ROOT="$HOME/.pyenv"'
-  echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"'
-  echo 'eval "$(pyenv init -)"'
-} >> ~/.bashrc
+pyenv_shell_config_snippet='export PYENV_ROOT="$HOME/.pyenv"'
 
-echoerr "adding pyenv environment variables to .profile"
+if ! grep "$pyenv_shell_config_snippet" ~/.bashrc; then 
+  echoerr 'adding pyenv environment variables to .bashrc'
+  # we want to put these literal strings (with their variables) into .bashrc
+  # without interpolating/evaluating the variables
+  # shellcheck disable=SC2016
+  {
+    echo 'export PYENV_ROOT="$HOME/.pyenv"'
+    echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"'
+    echo 'eval "$(pyenv init -)"'
+  } >> ~/.bashrc
+fi
+
+if ! grep "$pyenv_shell_config_snippet" ~/.profile; then 
+  echoerr "adding pyenv environment variables to .profile"
+  # we want to put these literal strings (with their variables) into .profile
+  # without interpolating/evaluating the variables
+  # shellcheck disable=SC2016
+  {
+    echo 'export PYENV_ROOT="$HOME/.pyenv"'
+    echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"'
+    echo 'eval "$(pyenv init -)"'
+  } >> ~/.profile
+fi
+
 # we want to put these literal strings (with their variables) into .profile
 # without interpolating/evaluating the variables
 # shellcheck disable=SC2016
-{
-  echo 'export PYENV_ROOT="$HOME/.pyenv"'
-  echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"'
-  echo 'eval "$(pyenv init -)"'
-} >> ~/.profile
+direnv_bashrc_snippet='eval "$(direnv hook bash)"'
+
+install_package_if_absent 'direnv'
+if ! grep "$direnv_bashrc_snippet" ~/.bashrc; then
+  echo "$direnv_bashrc_snippet" >> ~/.bashrc
+fi
+echoerr 'done installing and loading direnv'
 
 # shellcheck disable=SC2016
 echoerr 'restarting the shell to pick up the changes to $PATH'
@@ -72,18 +97,20 @@ echoerr 'restarting the shell to pick up the pyenv changes'
 # shellcheck disable=1091
 source "$HOME/.bashrc"
 
-echoerr "installing docker"
-curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
-
-echoerr "done installing docker. adding user \"${USER}\" to the docker group"
-if [ "$(getent group docker)" ]; then
-  echoerr 'docker group already exists'
-else
-  echoerr 'docker group does not exist. adding it now'
-  sudo groupadd docker
+if ! command -v docker; then
+  echoerr "installing docker"
+  curl -fsSL https://get.docker.com -o get-docker.sh
+  sudo sh get-docker.sh
+  
+  echoerr "done installing docker. adding user \"${USER}\" to the docker group"
+  if [ "$(getent group docker)" ]; then
+    echoerr 'docker group already exists'
+  else
+    echoerr 'docker group does not exist. adding it now'
+    sudo groupadd docker
+  fi
+  sudo usermod -aG docker "$USER"
+  newgrp docker
+  
+  echoerr "done setting up docker. reboot now"
 fi
-sudo usermod -aG docker "$USER"
-newgrp docker
-
-echoerr "done setting up docker. reboot now"
