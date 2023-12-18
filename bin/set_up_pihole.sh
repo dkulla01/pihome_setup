7bf006e81c1ae3def19ae3e7d361402b0c12f164
Daniel Kulla <dkulla129@gmail.com>
2023-12-18 18:10:03 -0500

    add a message about DNS
    I think that the DNS settings in resolvconf.conf must have been
    overwritten at some point (likely by me), leading to hass -> MQTT
    connections being kinda busted

--- bin/set_up_pihole.sh
+++ bin/set_up_pihole.sh
@@ -6,11 +6,13 @@ DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 # shellcheck source=./echoerr.sh
 source "$DIR/echoerr.sh"
 
+# todo: if we point to cloudflare, then to pihole (on localhost),
+# rerunning this script will overwrite our config to use local DNS
 echoerr 'pointing nameserver to a public nameserver'
 if grep -q 'name_servers' /etc/resolvconf.conf; then
-  sudo sed -i -E 's/^.*name_servers=.*$/name_servers=1.1.1.1/' /etc/resolvconf.conf
+  sudo sed -i -E 's/^.*name_servers=.*$/name_servers=1.1.1.1/ # point to cloudflare DNS' /etc/resolvconf.conf
 else
-  echo 'name_servers=127.0.0.1' | sudo tee -a /etc/resolvconf.conf > /dev/null
+  echo 'name_servers=127.0.0.1 # point to localhost (pihole)' | sudo tee -a /etc/resolvconf.conf > /dev/null
 fi
 
 sudo resolvconf -u
