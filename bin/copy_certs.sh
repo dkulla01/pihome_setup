5d20b83f941555b2724ca65ebff2d4c154daaabe
Daniel Kulla <dkulla129@gmail.com>
2023-12-26 18:13:39 -0500

    can we make traefik use the new certs?

--- bin/copy_certs.sh
+++ bin/copy_certs.sh
@@ -77,9 +77,56 @@ function check_mqtt_client_certs() {
 
 
 echoerr "checking for the mqtt client certs"
+zigbee2mqtt_mqtt_client_name='zigbee2mqtt-mqtt-client'
+zigbee2mqtt_mqtt_client_name_present=false
 for mqtt_client in "${all_mqtt_clients[@]}"; do
   check_mqtt_client_certs "$mqtt_client"
+  if [[ "$mqtt_client" == "$zigbee2mqtt_mqtt_client_name" ]]; then
+    zigbee2mqtt_mqtt_client_name_present=true
+  fi
 done
+
+if [[ ! $zigbee2mqtt_mqtt_client_name_present ]]; then
+  echoerr "${zigbee2mqtt_mqtt_client_name} must be present in mqtt-clients.json config file"
+  exit 1
+fi
 
 
 # at this point, all of the certs and keys we want exist and look good, so let's start copying them.
+docker_project_dir="${project_dir}/docker"
+traefik_root_cert_mount_source="${docker_project_dir}/ssl/root-cert-${ROOT_CERT_VERSION}"
+traefik_cert_mount_source="${docker_project_dir}/ssl/cert-${SSL_CERT_VERSION}"
+
+echoerr "copying certificates for traefik reverse proxy"
+echoerr "copying root certificate for traefik reverse proxy"
+if [[ ! -d "$traefik_root_cert_mount_source" ]]; then
+  mkdir -p "$traefik_root_cert_mount_source"
+fi
+
+cp "$root_cert_file" "$traefik_root_cert_mount_source"
+
+echoerr "copying non-root certs for traefik reverse proxy"
+mkdir -p "$traefik_cert_mount_source"
+
+cp "$traefik_cert_file" "$traefik_cert_key_file" "$traefik_cert_mount_source"
+
+
+zigbee2mqtt_dir="${docker_project_dir}/zigbee2mqtt"
+mosquitto_ssl_dir="${zigbee2mqtt_dir}/mosquitto-ssl-${SSL_CERT_VERSION}"
+zigbee2mqtt_ssl_dir="${zigbee2mqtt_dir}/zigbee2mqtt-ssl-${SSL_CERT_VERSION}"
+
+echoerr "copying mosquitto server certs"
+mkdir -p "$mosquitto_ssl_dir"
+
+cp "$mqtt_server_cert_file" "$mqtt_server_cert_key_file" "$mosquitto_ssl_dir"
+
+echoerr "copying zigbee2mqtt client certs"
+mkdir -p "$zigbee2mqtt_ssl_dir"
+zigbee2mqtt_cert_dir="${versioned_cert_dir}/${zigbee2mqtt_mqtt_client_name}"
+zigbee2mqtt_cert_file="${zigbee2mqtt_cert_dir}/${zigbee2mqtt_mqtt_client_name}.crt"
+zigbee2mqtt_key_file="${zigbee2mqtt_cert_dir}/${zigbee2mqtt_mqtt_client_name}.key"
+cp "$zigbee2mqtt_cert_file" "$zigbee2mqtt_key_file" "$zigbee2mqtt_ssl_dir"
+
+
+echoerr "done copying all of the certs that can be automatically copied. You still \
+need to manually add mqtt client certificates and keys to mqtt-explorer and homeassistant."
