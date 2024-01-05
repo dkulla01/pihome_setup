350ff9906c0e642dfe2c486fe9556ab9845e870c
Daniel Kulla <dkulla129@gmail.com>
2024-01-05 13:42:29 -0500

    bash var expansion/indirection is hard

--- bin/generate_certs.sh
+++ bin/generate_certs.sh
@@ -225,7 +225,7 @@ jq --raw-output '.[]' "$mqtt_client_list_file" | while read -r mqtt_client_name;
 done
 
 extra_mqtt_clients_env_var="EXTRA_MQTT_CLIENTS"
-if [[ -n "$extra_mqtt_clients_env_var" ]]; then
+if [[ -n "${!extra_mqtt_clients_env_var}" ]]; then
   echoerr "creating extra mqtt clients specified by ${extra_mqtt_clients_env_var}"
   # add an extra comment to end of the env var value to make sure we
   # capture the last value and nix the trailing newline
